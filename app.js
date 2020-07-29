a8238be198a070a84af06f8a8204a7b3e09bfd93
Brian Ratunil <brianra2nil@hotmail.com>
2020-07-28 20:03:58 -0700

    updated

--- app.js
+++ app.js
@@ -13,21 +13,24 @@ let questionCounter = 0
 
 function showQ() {
     if (questionCounter < questions.length) {
-    let question = Object.keys(questions[questionCounter])
-    console.log(question)
-    document.getElementById("showq").textContent = question
-    document.getElementById("result").innerHTML = "" }//added this line to make the green and red boxes go away when next question
-else {document.createElement = ('div')
-className = 'leaderboards'
-innerHTML = `
-<label>enter name</label>
-<input></input>
-<button>Submist score</button>
+        let question = Object.keys(questions[questionCounter])
+        console.log(question)
+        document.getElementById("showq").textContent = question
+        document.getElementById("result").innerHTML = ""
+    }//added this line to make the green and red boxes go away when next question
+    else {
+        
+        document.getElementById('leaderboards').innerHTML = `
+        <form>
+        <label>Game Over!</label>
+<input class="username" placeholder="Enter Name"></input>
+<button id="submitBtn">Submit score</button>
+</form>
 `
 
 
 
-}
+    }
 }
 
 // function for the start quiz button
@@ -53,9 +56,9 @@ function handleStart(event) {
             count = 120
             startDiv.style.display = "block"
             startButtons.style.display = "none"
-            
+
         }
-        
+
         document.getElementById('score').textContent = score
         document.getElementById("div1").textContent = count
 
@@ -74,7 +77,7 @@ document.getElementById('true').addEventListener('click', () => {
     if (questionCounter >= questions) return
     let answer = Object.values(questions[questionCounter])
     if (answer == 'true') {
-        score+=10
+        score += 10
         document.getElementById('result').innerHTML = `
 <div class="alert alert-success" role="alert" id="largeFont">
 Right!!
@@ -82,14 +85,14 @@ Right!!
 
 `
     } else {
-        count -= 10      
+        count -= 10
         document.getElementById('result').innerHTML = `
 <div class="alert alert-danger" role="alert"id="largeFont">
 Wrong!! 
 </div>
 
 
-`   
+`
 
     }
     let delayer = setTimeout(() => {
@@ -103,10 +106,10 @@ Wrong!!
 
 document.getElementById('false').addEventListener('click', () => {
     if (questionCounter >= questions) return
-  
+
     let answer = Object.values(questions[questionCounter])
     if (answer == 'false') {
-        score+=10
+        score += 10
         document.getElementById('result').innerHTML = `
 <div class="alert alert-success" role="alert" id="largeFont">
 Right!!
@@ -121,9 +124,9 @@ Right!!
 Wrong!! 
 </div>
 
-`   
+`
     }
-    
+
     let delayer = setTimeout(() => {
         questionCounter += 1
         clearTimeout(delayer)
@@ -133,7 +136,47 @@ Wrong!!
 })
 
 
+const submitScore = submission => {
+    console.log(submission)
+    
+    let leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || []
+  
+    leaderboard.push(submission)
+  
+    localStorage.setItem('leaderboard', JSON.stringify(leaderboard))
+  
+    leaderboard.sort((a, b) => {
+      return b.score - a.score
+    })
 
-localStorage.setItem('score', '${score}')
+
+let bodyElem = document.createElement('tbody')
+
+  for (let i = 0; i < leaderboard.length; i++) {
+    let rowElem = document.createElement('tr')
+    rowElem.innerHTML = `
+      <th scope="row">${i + 1}</th>
+      <td>${leaderboard[i].username}</td>
+      <td>${leaderboard[i].score}</td>
+    `
+    bodyElem.append(rowElem)
+  }
+
+  tableElem.append(bodyElem)
+
+  document.getElementById('trivia').append(tableElem)
 
 
+
+}
+document.getElementById('submitBtn').addEventListener('click', event => {
+    if (event.target.classList.contains('answer')) {
+      getAnswer(event.target.dataset.answer)
+    } else if (event.target.id === 'submitScore') {
+      event.preventDefault()
+      submitScore({
+        username: document.getElementById('username').value,
+        score: score
+      })
+    }
+  })
